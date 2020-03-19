733cf39e24b726831001ecf3fc54188335bc276d
Vinh Hoang <nhuvinh.hoang@gmail.com>
2020-03-19 09:47:06 +0700

    Add Inheritance.Lab-Part 1

---
+++ Inheritance.Lab/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance.Lab
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Dog myDog = new Dog();
+			myDog.Eat();
+			myDog.Bark();
+
+			Console.ReadLine();
+
+			Cat myCat = new Cat();
+			myCat.Eat();
+			myCat.Meow();
+
+			Console.ReadLine();
+
+			Puppy myPuppy = new Puppy();
+
+			myPuppy.Eat();
+			myPuppy.Bark();
+			myPuppy.Weep();
+
+			Console.ReadLine();
+
+		}
+	}
+}
