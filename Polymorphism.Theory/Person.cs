c976f561de14e22ee1dd5d0be8e2de2a79a0433c
Vinh Hoang <nhuvinh.hoang@gmail.com>
2020-03-24 09:14:16 +0700

    Add Polymorphism.Theory Overloading

---
+++ Polymorphism.Theory/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism.Theory
+{
+	public class Person
+	{
+		public void Run()
+		{
+			Console.WriteLine("This person is running 100 meters");
+		}
+
+		public void Run(int distance)
+		{
+			Console.WriteLine("This person is running " + distance + " meters");
+		}
+
+		public void Run(int distance, IVehicle vehicle)
+		{
+			Console.WriteLine("This person is running very fast " + distance + " meters");
+			vehicle.Run();
+		}
+
+		public string Run(IVehicle vehicle)
+		{
+			return "The vehicle is running ...";
+		}
+	}
+}
---
+++ Polymorphism.Theory/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism.Theory
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Person myPerson = new Person();
+			Car car = new Car();
+
+			myPerson.Run();
+			myPerson.Run(1000);
+			myPerson.Run(10000, car);
+			Console.ReadLine();
+
+			Console.WriteLine(myPerson.Run(car));
+			Console.ReadLine();
+
+		}
+	}
+}
