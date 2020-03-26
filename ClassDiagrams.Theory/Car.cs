c09b70cadf216fcab9d4686269aca8034fb70189
Vinh Hoang <nhuvinh.hoang@gmail.com>
2020-03-26 11:32:33 +0700

    Add ClassDiagrams.Theory part 3

---
+++ ClassDiagrams.Theory/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDiagrams.Theory
+{
+	public class Car
+	{
+		public Engine Engine;
+
+		public Car(Engine engine)
+		{
+			Engine = engine;
+		}
+	}
+}
--- ClassDiagrams.Theory/Program.cs
+++ ClassDiagrams.Theory/Program.cs
@@ -17,6 +17,15 @@ namespace ClassDiagrams.Theory
 			Console.WriteLine("TotalStudents: " + Student.getTotalStudent());
 
 			Console.ReadLine();
+
+			Engine engine_1 = new Engine();
+			Engine engine_2 = new Engine();
+
+			Car car_1 = new Car(engine_1);
+			car_1.Engine = engine_2;
+			Car car_2 = new Car(engine_1);
+
+			Book book = new Book();
 		}
 	}
 }
