e888cca494cc8cc678d313a4503b6bbf91de1ade
Vinh Hoang <nhuvinh.hoang@gmail.com>
2020-03-26 10:26:01 +0700

    Add ClassDiagrams.Theory Part 1

---
+++ ClassDiagrams.Theory/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDiagrams.Theory
+{
+	class Rectangle
+	{
+		private int width;
+		private int height;
+		private double area;
+
+		public Rectangle(int h, int w)
+		{
+
+		}
+
+		public double distance(Rectangle r)
+		{
+			// To be implemeted
+			return 0;
+		}
+	}
+}
---
+++ ClassDiagrams.Theory/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDiagrams.Theory
+{
+	class Student
+	{
+		private string name;
+		private int id;
+		static private int totalStudents;
+
+		protected int getId()
+		{
+			// To be implemented
+			return 0;
+		}
+
+		string getEmail()
+		{
+			// To be implemented
+			return "";
+		}
+	}
+}
