69ab29878383d6d3a1076820a1b0a4df0c617d24
Vinh Hoang <nhuvinh.hoang@gmail.com>
2020-03-31 11:16:55 +0700

    Add Abstraction.Lab Problem 1

---
+++ Abstraction.Lab/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction.Lab
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Point point_1 = new Point(1, 1);
+			Point point_2 = new Point(5, 3);
+
+			Rectangle rectangle = new Rectangle(-2, 3, 3, 1);
+
+			Console.WriteLine(rectangle.Contains(point_1)); // Return true
+			Console.WriteLine(rectangle.Contains(point_2)); // Return false
+
+			Console.ReadLine();
+
+		}
+	}
+}
---
+++ Abstraction.Lab/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction.Lab
+{
+	class Rectangle
+	{
+		Point topLeft;
+		Point bottomRight;
+
+		public Rectangle(int topLeftX, int topLeftY, int bottomRightX,
+			int bottomRightY)
+		{
+			topLeft = new Point(topLeftX, topLeftY);
+			bottomRight = new Point(bottomRightX, bottomRightY);
+		}
+
+		public bool Contains(Point point)
+		{
+			return point.X >= topLeft.X && point.X <= bottomRight.X
+				&& point.Y <= topLeft.Y && point.Y >= bottomRight.Y;
+		}
+	}
+}
