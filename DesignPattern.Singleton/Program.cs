d7ea333735effb252bb2cf2ca7f07f06a28e89d2
Vinh Hoang <nhuvinh.hoang@gmail.com>
2020-04-07 08:19:57 +0700

    Add Singleton pattern

---
+++ DesignPattern.Singleton/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Singleton
+{
+	class President
+	{
+		private static President instance;
+
+		private President()
+		{
+
+		}
+
+		public static President GetInstance()
+		{
+			if (instance != null) return instance;
+			else
+			{
+				instance = new President();
+				return instance;
+			}
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			President president_1 = President.GetInstance();
+
+			President president_2 = President.GetInstance();
+
+			Console.WriteLine(president_1.Equals(president_2));
+
+			Console.ReadLine();
+		}
+	}
+}
