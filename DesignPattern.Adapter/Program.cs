a41fed9b6592b37c29732532e979739556f83b6c
Vinh Hoang <nhuvinh.hoang@gmail.com>
2020-04-07 10:24:05 +0700

    Add Adapter pattern

---
+++ DesignPattern.Adapter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Adapter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			AfricanLion africanLion = new AfricanLion();
+			AsianLion asianLion = new AsianLion();
+
+			Hunter hunter = new Hunter();
+			hunter.Hunt(africanLion);
+			hunter.Hunt(asianLion);
+
+			Wolf wolf = new Wolf();
+			WolfAdapter wolfAdapter = new WolfAdapter(wolf);
+			hunter.Hunt(wolfAdapter);
+		}
+	}
+}
---
+++ DesignPattern.Adapter/WolfAdapter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Adapter
+{
+	class WolfAdapter : ILion
+	{
+		private Wolf _wolf;
+		public WolfAdapter(Wolf wolf)
+		{
+			_wolf = wolf;
+		}
+	}
+}
