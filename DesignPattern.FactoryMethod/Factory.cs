f158830a634f973a6984f0cfcda9f9c105fbfbd7
Vinh Hoang <nhuvinh.hoang@gmail.com>
2020-04-07 08:42:27 +0700

    Add Factory method pattern

---
+++ DesignPattern.FactoryMethod/Factory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.FactoryMethod
+{
+	class Factory
+	{
+		public IProduct CreateCar()
+		{
+			return new Car();
+		}
+
+		public IProduct CreateTable()
+		{
+			return new Table();
+		}
+
+		public IProduct CreateChair()
+		{
+			return new Chair();
+		}
+
+		public IProduct CreateBicycle()
+		{
+			return new Bicycle();
+		}
+	}
+}
---
+++ DesignPattern.FactoryMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.FactoryMethod
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Factory factory = new Factory();
+
+			IProduct car = factory.CreateCar();
+
+			IProduct table = factory.CreateTable();
+
+			IProduct chair = factory.CreateChair();
+		}
+	}
+}
